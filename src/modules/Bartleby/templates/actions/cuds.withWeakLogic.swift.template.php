4082b83fc1bd7b4477d1b5f7062224b9f1051229
Benoit Pereira da Silva <bpds@me.com>
2015-12-03 10:01:19 +0100

    Swift Template Cuds With Weak logic implementation

--- src/modules/Bartleby/templates/actions/cuds.withWeakLogic.swift.template.php
+++ src/modules/Bartleby/templates/actions/cuds.withWeakLogic.swift.template.php
@@ -17,17 +17,23 @@ if (isset( $f,$d,$h)) {
     /* @var $d ActionRepresentation*/
     /* @var $h Hypotypose */
 
+
     // We use explicit name.
     $flexed=$f;
     $actionRepresentation=$d;
+    $actionRepresentation->class=$actionRepresentation->class;
     $hypotypose=$h;
 
-    $flexed->fileName = "TMP".$actionRepresentation->class . '.swift';
+    $flexed->fileName = $actionRepresentation->class . '.swift';
     $flexed->package = 'xOS/endpoints/';
 
 }else{
     return NULL;
 }
+
+/////////////////
+// EXCLUSIONS
+/////////////////
 
 // Should this Action be excluded ?
 
@@ -40,47 +46,96 @@ if (isset($excludeActionsWith)) {
     }
 }
 
-// Compute ALL the Variables you need in the template
-
-$httpMethod=$actionRepresentation->httpMethod;
-$singularName=lcfirst($actionRepresentation->class);
-$pluralizedName=lcfirst(Pluralization::pluralize($actionRepresentation->class));
-$ucfSingularName=ucfirst($singularName);
-$ucfPluralizedName=ucfirst($pluralizedName);
-
-$successP = $d->getSuccessResponse();
-$successTypeString = '';
-if ($successP->type == FlexionsTypes::COLLECTION) {
-    $successTypeString = '['.$successP->instanceOf.']';
-} else if ($successP->type == FlexionsTypes::OBJECT) {
-    $successTypeString = ucfirst($successP->instanceOf);
-} else if ($successP->type == FlexionsTypes::DICTIONARY) {
-    $successTypeString = 'Dictionary<String, AnyObject>';
-}else {
-    $nativeType = FlexionsSwiftLang::nativeTypeFor($successP->type);
-    if($nativeType==FlexionsTypes::NOT_SUPPORTED){
-        $successTypeString='';
-    }else{
-        $successTypeString=$nativeType;
-    }
-}
-
 
 // This template cannot be used for GET Methods
-if ($httpMethod=="GET"){
+if ($actionRepresentation->httpMethod==='GET'){
+    return NULL;
+}
+
+// We want also to exclude by query
+
+if (!(strpos($d->class,'ByQuery')===false)){
     return NULL;
 }
 
 
+/////////////////////////
+// VARIABLES COMPUTATION
+/////////////////////////
+
+
+if ($actionRepresentation->class=="DeleteUser"){
+    $a='b';
+}
+
+// Compute ALL the Variables you need in the template
+
+$httpMethod=$actionRepresentation->httpMethod;
+$pluralizedName=lcfirst($actionRepresentation->collectionName);
+$singularName=lcfirst(Pluralization::singularize($pluralizedName));
+$baseClassName=ucfirst($actionRepresentation->class);
+$ucfSingularName=ucfirst($singularName);
+$ucfPluralizedName=ucfirst($pluralizedName);
+
+$actionString='creation';
+$localAction='upsert';
+
+if ($httpMethod=="POST"){
+    $actionString='creation';
+    $localAction='upsert';
+}elseif ($httpMethod=="PUT"){
+    $actionString='update';
+    $localAction='upsert';
+}elseif ($httpMethod=="PATCH"){
+    $actionString='update';
+    $localAction='upsert';
+}elseif ($httpMethod=="DELETE"){
+    $actionString='delete';
+    $localAction='delete';
+}else{
+    $actionString='NO_FOUND';
+    $localAction='NO_FOUND';
+}
+
+$firstParameterName=NULL;
+$firstParameterTypeString=NULL;
+$varName=NULL;
+$successTypeString = NULL;
+
+while($actionRepresentation->iterateOnParameters()){
+    /*@var $parameter PropertyRepresentation*/
+    $parameter=$actionRepresentation->getParameter();
+    // We use the first parameter.
+    if (!isset($varName,$successTypeString)){
+        if ($parameter->type == FlexionsTypes::COLLECTION){
+            $firstParameterName=$parameter->name;
+            $firstParameterTypeString=($httpMethod!="DELETE")?'['.$ucfSingularName.']':"String";
+            $varName=$pluralizedName;
+            $successTypeString = '['.$ucfSingularName.']';
+        }else{
+            $firstParameterName=$parameter->name;
+            $firstParameterTypeString=($httpMethod!="DELETE")?$ucfSingularName:"String";
+            $varName=$singularName;
+            $successTypeString =$ucfSingularName;
+        }
+    }
+}
+
+
+/////////////////////////
+// TEMPLATE
+/////////////////////////
+
 /* TEMPLATES STARTS HERE -> */?>
 <?php echo GenerativeHelperForSwift::defaultHeader($flexed,$actionRepresentation); ?>
+
 import Foundation
 import Alamofire
 import ObjectMapper
 
-@objc(CreateSampleParameters) class CreateSampleParameters : JObject {
+@objc(<?php echo $baseClassName ?>Parameters) class <?php echo $baseClassName ?>Parameters : JObject {
 
-    var sample:Sample?
+    var <?php echo $firstParameterName ?>:<?php echo $firstParameterTypeString ?>?
 
     required init(){
         super.init()
@@ -95,14 +150,14 @@ import ObjectMapper
 
     override func mapping(map: Map) {
         super.mapping(map)
-        sample <- map["sample"]
+        <?php echo$firstParameterName ?> <- map["<?php echo$firstParameterName ?>"]
     }
 }
 
 
-@objc(CreateSample) class CreateSample : JObject{
+@objc(<?php echo$baseClassName ?>) class <?php echo$baseClassName ?> : JObject{
 
-    private var _parameters:CreateSampleParameters
+    private var _parameters:<?php echo$baseClassName ?>Parameters
 
     private var _documentUDID:String
 
@@ -113,7 +168,7 @@ import ObjectMapper
     private var _invocation:Invocation=Invocation()
 
     required convenience init(){
-        self.init(within:"",parameters:CreateSampleParameters(),rUDID:"",observableViaUDID:nil)
+        self.init(within:"",parameters:<?php echo$baseClassName ?>Parameters(),rUDID:"",observableViaUDID:nil)
     }
 
     required convenience init?(_ map: Map) {
@@ -140,7 +195,7 @@ import ObjectMapper
     - parameter observableViaUDID: If you want to support distributed execution this action will be propagated with this UDID
 
     */
-    init (within documentUDID:String, parameters:CreateSampleParameters,rUDID:String,observableViaUDID:String?) {
+    init (within documentUDID:String, parameters:<?php echo$baseClassName ?>Parameters,rUDID:String,observableViaUDID:String?) {
         self._parameters=parameters
         self._documentUDID=documentUDID
         self._observableViaUDID=observableViaUDID
@@ -150,53 +205,50 @@ import ObjectMapper
 
     func commit(){
         // This first test should not be necessary (with BARTLEBY 1.0)
-        if let sample=self._parameters.sample{
+        if let <?php echo$firstParameterName ?>=self._parameters.<?php echo$firstParameterName ?>{
 
-            let context=Context(code:0, caller: "CreateSample.commit")
+            let context=Context(code:0, caller: "<?php echo$baseClassName ?>.commit")
 
             if let registry = Bartleby.sharedInstance.getRegistryByUDID(self._documentUDID) {
+                // <?php echo$localAction ?> locally
+                <?php if ($httpMethod!="DELETE") {
+                    echo("if registry.$localAction($firstParameterName){");
+                } else {
+                    echo("if registry.$localAction($firstParameterName, fromCollectionWithName:\"$actionRepresentation->collectionName\"){"); }
+                ?>
+                    // Prepare the invocation
+                    self._invocation.counter=0
+                    self._invocation.status=Invocation.Status.Pending
 
-                // Upsert locally
-                if registry.upsert(sample){
-
-                // Prepare the invocation
-                self._invocation.counter=0
-                self._invocation.status=Invocation.Status.Pending
-
-                // Provision the invocation.
-                do{
-                let ic:InvocationsCollectionController = try registry.getCollection()
-                ic.add(self._invocation)
-                }catch{
-
-                Bartleby.sharedInstance.dispatchAdaptiveMessage(context,
-                title: "Structural Error",
-                body: "Invocation collection is missing",
-                onSelectedIndex: { (selectedIndex) -> () in
-                })
-
+                    // Provision the invocation.
+                    do{
+                        let ic:InvocationsCollectionController = try registry.getCollection()
+                        ic.add(self._invocation)
+                    }catch{
+                        Bartleby.sharedInstance.dispatchAdaptiveMessage(context,
+                        title: "Structural Error",
+                        body: "Invocation collection is missing",
+                        onSelectedIndex: { (selectedIndex) -> () in
+                        })
+                    }
+                    // The status will mark Invocation.hasChanged as true
+                    self._invocation.data=self.dictionaryRepresentation()
+                }else{
+                    // Its is a Local Failure.
+                    Bartleby.sharedInstance.dispatchAdaptiveMessage(context,
+                        title:NSLocalizedString("Dynamic Error", comment: "Dynamic Error"),
+                        body: NSLocalizedString("Local operation has failed", comment: "Local operation has failed"),
+                        onSelectedIndex: { (selectedIndex) -> () in
+                    })
                 }
-                // The status will mark Invocation.hasChanged as true
-                self._invocation.data=self.dictionaryRepresentation()
-
 
             }else{
-                // Its is a Local Failure.
+                // This registry is not available there is nothing to todo.
                 Bartleby.sharedInstance.dispatchAdaptiveMessage(context,
-                title:NSLocalizedString("Dynamic Error", comment: "Dynamic Error"),
-                body: NSLocalizedString("Local operation has failed", comment: "Local operation has failed"),
-                onSelectedIndex: { (selectedIndex) -> () in
+                    title: NSLocalizedString("Structural error", comment: "Structural error"),
+                    body: NSLocalizedString("Registry is missing", comment: "Registry is missing"),
+                    onSelectedIndex: { (selectedIndex) -> () in
                 })
-            }
-
-            }else{
-            // This registry is not available there is nothing to todo.
-
-            Bartleby.sharedInstance.dispatchAdaptiveMessage(context,
-            title: NSLocalizedString("Structural error", comment: "Structural error"),
-            body: NSLocalizedString("Registry is missing", comment: "Registry is missing"),
-            onSelectedIndex: { (selectedIndex) -> () in
-            })
             }
         }else{
             // TEMP this situation should not occur with BARTLEBY 1.0
@@ -204,45 +256,50 @@ import ObjectMapper
     }
 
     func push(){
-        // The unitary operation are not always idempotent
-        // so we do not want to push multiple times unintensionnaly.
-        if  self._invocation.status==Invocation.Status.Pending ||
-            self._invocation.status==Invocation.Status.Unsucessful {
-            // We try to execute
-            self._callAPI(self._documentUDID,
-                parameters: self._parameters,
-                sucessHandler: { () -> () in
-                    self._parameters.sample?.hasBeenDistributed=true
-                    // In any case we must mark the invocation has successful.
-                    self._invocation.counter=self._invocation.counter!+1
-                    self._invocation.status=Invocation.Status.Successful
-                },
+        if let <?php if($httpMethod!="DELETE"){echo("registry");}else{echo("_");} ?> = Bartleby.sharedInstance.getRegistryByUDID(self._documentUDID) {
+            // The unitary operation are not always idempotent
+            // so we do not want to push multiple times unintensionnaly.
+            if  self._invocation.status==Invocation.Status.Pending ||
+                self._invocation.status==Invocation.Status.Unsucessful {
+                // We try to execute
+                self._invocation.status=Invocation.Status.InProgress
+                <?php echo$baseClassName ?>.execute(self._documentUDID,
+                    parameters: self._parameters,
+                    sucessHandler: { () -> () in
+                        <?php if ($httpMethod=="POST") {
+                            echo("registry.markAsDistributed(self._parameters.$firstParameterName)".cr());
+                        } else {
+                            echo(cr());
+                        }
+                        ?>
+                        self._invocation.counter=self._invocation.counter!+1
+                        self._invocation.status=Invocation.Status.Successful
+                    },
                     failureHandler: {(result: HTTPContext) -> () in
-                    self._invocation.counter=self._invocation.counter!+1
-                    self._invocation.status=Invocation.Status.Unsucessful
-                    self._invocation.failureMessage="\(result)"
-                }
-            )
-        }else{
-            // This registry is not available there is nothing to todo.
-            let context=Context(code:0, caller: "CreateSample.push")
-            Bartleby.sharedInstance.dispatchAdaptiveMessage(context,
-                title: NSLocalizedString("Push error", comment: "Push error"),
-                body: "\(NSLocalizedString("Attempt to push an invocation with status ==",comment:"Attempt to push an invocation with status =="))\(self._invocation.status))",
-                onSelectedIndex: { (selectedIndex) -> () in
+                        self._invocation.counter=self._invocation.counter!+1
+                        self._invocation.status=Invocation.Status.Unsucessful
+                        self._invocation.failureMessage="\(result)"
+                    }
+                )
+            }else{
+                // This registry is not available there is nothing to todo.
+                let context=Context(code:0, caller: "<?php echo$baseClassName ?>.push")
+                Bartleby.sharedInstance.dispatchAdaptiveMessage(context,
+                    title: NSLocalizedString("Push error", comment: "Push error"),
+                    body: "\(NSLocalizedString("Attempt to push an invocation with status ==",comment:"Attempt to push an invocation with status =="))\(self._invocation.status))",
+                    onSelectedIndex: { (selectedIndex) -> () in
                 })
-
+            }
         }
     }
 
-    private func _callAPI(rUDID:String,
-            parameters:CreateSampleParameters,
+    static func execute(rUDID:String,
+            parameters:<?php echo$baseClassName ?>Parameters,
             sucessHandler success:()->(),
             failureHandler failure:(context:HTTPContext)->()){
-                self._invocation.status=Invocation.Status.InProgress
-                let pathURL=Configuration.baseUrl.URLByAppendingPathComponent("/sample")
+                let pathURL=Configuration.baseUrl.URLByAppendingPathComponent("/<?php echo$varName ?>")
                 let dictionary:Dictionary<String, AnyObject>?=Mapper().toJSON(parameters)
-                let urlRequest=HTTPManager.mutableRequestWithToken(relatedUDID:rUDID,withActionName:"CreateSample" ,forMethod:Method.POST, and: pathURL)
+                let urlRequest=HTTPManager.mutableRequestWithToken(relatedUDID:rUDID,withActionName:"<?php echo$baseClassName ?>" ,forMethod:Method.<?php echo$httpMethod?>, and: pathURL)
                 let r:Request=request(ParameterEncoding.JSON.encode(urlRequest, parameters: dictionary).0)
                 r.responseString{ response in
 
@@ -253,7 +310,7 @@ import ObjectMapper
                     // Bartleby consignation
 
                     let context = HTTPContext( code: 1000,
-                        caller: "CreateSamples._callApi",
+                        caller: "<?php echo$baseClassName ?>._callApi",
                         relatedURL:request?.URL,
                         httpStatusCode: response?.statusCode ?? 0,
                         response: response )
@@ -267,41 +324,39 @@ import ObjectMapper
                             context: context,
                             title: NSLocalizedString("Unsuccessfull attempt",
                             comment: "Unsuccessfull attempt"),
-                            body: NSLocalizedString("creation of samples",
-                            comment: "creation of samples failure description"),
+                            body: NSLocalizedString("<?php echo$actionString ?>  of <?php echo$varName ?>",
+                            comment: "<?php echo$actionString ?> of <?php echo$varName ?> failure description"),
                             trigger:{ (selectedIndex) -> () in
                             print("Post presentation message selectedIndex:\(selectedIndex)")
                         })
                         reactions.append(failureReaction)
                         failure(context:context)
                     }else{
-                    if let statusCode=response?.statusCode {
-                        if 200...299 ~= statusCode {
-                            success()
-                        }else{
-                            // Bartlby does not currenlty discriminate status codes 100 & 101
-                            // and treats any status code >= 300 the same way
-                            // because we consider that failures differentiations could be done by the caller.
-                            let failureReaction =  Bartleby.Reaction.DispatchAdaptiveMessage(
-                                context: context,
-                                title: NSLocalizedString("Unsuccessfull attempt",
-                                comment: "Unsuccessfull attempt"),
-                                body: NSLocalizedString("creation of samples",
-                                comment: "creation of samples failure description"),
-                                trigger:{ (selectedIndex) -> () in
-                                print("Post presentation message selectedIndex:\(selectedIndex)")
-                            })
-                            reactions.append(failureReaction)
-                            failure(context:context)
+                        if let statusCode=response?.statusCode {
+                            if 200...299 ~= statusCode {
+                                success()
+                            }else{
+                                // Bartlby does not currenlty discriminate status codes 100 & 101
+                                // and treats any status code >= 300 the same way
+                                // because we consider that failures differentiations could be done by the caller.
+                                let failureReaction =  Bartleby.Reaction.DispatchAdaptiveMessage(
+                                    context: context,
+                                    title: NSLocalizedString("Unsuccessfull attempt",
+                                    comment: "Unsuccessfull attempt"),
+                                    body: NSLocalizedString("<?php echo$actionString ?> of <?php echo$varName ?>",
+                                    comment: "<?php echo$actionString ?> of <?php echo$varName ?> failure description"),
+                                    trigger:{ (selectedIndex) -> () in
+                                    print("Post presentation message selectedIndex:\(selectedIndex)")
+                                })
+                                reactions.append(failureReaction)
+                                failure(context:context)
+                            }
                         }
-                    }
+                     }
+                    //Let's react according to the context.
+                    Bartleby.sharedInstance.perform(reactions, forContext: context)
                 }
-                //Let's react according to the context.
-                Bartleby.sharedInstance.perform(reactions, forContext: context)
             }
-
-    }
-
-
+    
 }
 <?php /*<- END OF TEMPLATE */?>
